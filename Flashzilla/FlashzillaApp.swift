3c62fed82fecea5a3103d83f1c40288fb45f3dc4
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-16 13:09:07 +0300

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Dmitry Sharabin on 16.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
