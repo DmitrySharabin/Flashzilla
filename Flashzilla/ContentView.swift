0c4d07eed0d0f96d03e5ff358102f885712abb33
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-16 13:40:34 +0300

    Day 86 — How to use gestures in SwiftUI

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,9 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @State private var currentAmount = 0.0
+//    @State private var finalAmount = 1.0
+    
+//    @State private var currentAmount = Angle.zero
+//    @State private var finalAmount = Angle.zero
+    
+    // how far the circle has been dragged
+    @State private var offset = CGSize.zero
+    
+    // whether it is currently being dragged or not
+    @State private var isDragging = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+//        Text("Hello, world!")
+//            .onTapGesture(count: 2) {
+//                print("Double tapped!")
+//            }
+        
+//            .onLongPressGesture {
+//                print("Long pressed!")
+//            }
+        
+//            .onLongPressGesture(minimumDuration: 2) {
+//                print("Long pressed!")
+//            }
+        
+//            .onLongPressGesture(minimumDuration: 1) {
+//                print("Long pressed!")
+//            } onPressingChanged: { inProgress in
+//                print("In progress: \(inProgress)!")
+//            }
+        
+//            .scaleEffect(finalAmount + currentAmount)
+//            .gesture(
+//                MagnificationGesture()
+//                    .onChanged { amount in
+//                        currentAmount = amount - 1
+//                    }
+//                    .onEnded { amount in
+//                        finalAmount += currentAmount
+//                        currentAmount = 0
+//                    }
+//            )
+        
+//            .rotationEffect(currentAmount + finalAmount)
+//            .gesture(
+//                RotationGesture()
+//                    .onChanged { angle in
+//                        currentAmount = angle
+//                    }
+//                    .onEnded { angle in
+//                        finalAmount += currentAmount
+//                        currentAmount = .zero
+//                    }
+//            )
+        
+//        VStack {
+//            Text("Hello, world!")
+//                .onTapGesture {
+//                    print("Text tapped!")
+//                }
+//        }
+//        .onTapGesture {
+//            print("VStack tapped!")
+//        }
+//        .highPriorityGesture(
+//            TapGesture()
+//                .onEnded { _ in
+//                    print("VStack tapped!")
+//                }
+//        )
+//        .simultaneousGesture(
+//            TapGesture()
+//                .onEnded { _ in
+//                    print("VStack tapped!")
+//                }
+//        )
+        
+        // a drag gesture that updates offset and isDragging as it moves around
+        let dragGesture = DragGesture()
+            .onChanged { value in offset = value.translation }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+        
+        // a long press gesture that enables isDragging
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+        
+        // a combined gesture that forces the user to long press then drag
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        Circle()
+            .fill(.red)
+            .frame(width: 64, height: 64)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
     }
 }
 
