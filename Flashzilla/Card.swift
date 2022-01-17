0a63ae6f038e42cef86375106c9bba091b1c64d3
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-17 20:16:21 +0300

    Day 88 — Designing a single card view

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Dmitry Sharabin on 17.01.2022.
+//
+
+import SwiftUI
+
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
