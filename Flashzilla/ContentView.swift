bfbde14ff21c78a4dd3dc4f003125f451c2d8a39
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-17 20:36:24 +0300

    Day 88 — Building a stack of cards

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,55 +7,32 @@
 
 import SwiftUI
 
-func withOptionalAnimation<Result>(_ animation: Animation? = .default, body: () throws -> Result) rethrows -> Result {
-    if UIAccessibility.isReduceMotionEnabled {
-        return try body()
-    } else {
-        return try withAnimation(animation, body)
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        
+        return self.offset(x: 0, y: offset * 10)
     }
 }
 
 struct ContentView: View {
-//    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
-    
-//    @Environment(\.accessibilityReduceMotion) var reduceMotion
-//    @State private var scale = 1.0
-    
-    @Environment(\.accessibilityReduceTransparency) var reduceTransparency
+    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
     
     var body: some View {
-//        HStack {
-//            if differentiateWithoutColor {
-//                Image(systemName: "checkmark.circle")
-//            }
-//
-//            Text("Success")
-//        }
-//        .padding()
-//        .background(differentiateWithoutColor ? .black : .green)
-//        .foregroundColor(.white)
-//        .clipShape(Capsule())
-        
-//        Text("Hello, World!")
-//            .scaleEffect(scale)
-//            .onTapGesture {
-////                if reduceMotion {
-////                    scale *= 1.5
-////                } else {
-//                    withAnimation {
-////                        scale *= 1.5
-////                    }
-////                }
-//                withOptionalAnimation {
-//                    scale *= 1.5
-//                }
-//            }
-        
-        Text("Hello, World!")
-            .padding()
-            .background(reduceTransparency ? .black: .black.opacity(0.5))
-            .foregroundColor(.white)
-            .clipShape(Capsule())
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
     }
 }
 
