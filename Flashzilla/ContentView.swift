66c0586aa10c0009f98c235e61a5cb8da2642995
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-01-18 15:09:02 +0300

    Day 89 — Ending the app with allowsHitTesting()

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -50,6 +50,15 @@ struct ContentView: View {
                         .stacked(at: index, in: cards.count)
                     }
                 }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundColor(.black)
+                        .clipShape(Capsule())
+                }
             }
             
             if differentiateWithoutColor {
@@ -84,7 +93,9 @@ struct ContentView: View {
         }
         .onChange(of: scenePhase) { newPhase in
             if newPhase == .active {
-                isActive = true
+                if cards.isEmpty == false {
+                    isActive = true                    
+                }
             } else {
                 isActive = false
             }
@@ -93,6 +104,16 @@ struct ContentView: View {
     
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func resetCards() {
+        cards = Array(repeating: Card.example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
